9176b35a8152f7062fbf94fe44d030cffc632bbb
Hamada Mohammed Seif <hamada.devlop2@gmail.com>
2025-10-21 01:33:44 +0300

    Animate the transitions

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:animated_responsive_layout/widgets/animated_floating_action_button.dart';
 import 'package:flutter/material.dart';
 
+import 'animations.dart';
 import 'destinations.dart';
 import 'models/data.dart' as data;
 import 'models/models.dart';
@@ -33,74 +35,109 @@ class Feed extends StatefulWidget {
   State<Feed> createState() => _FeedState();
 }
 
-class _FeedState extends State<Feed> {
+class _FeedState extends State<Feed> with SingleTickerProviderStateMixin{
   late final _colorScheme = Theme.of(context).colorScheme;
   late final _backgroundColor = Color.alphaBlend(
     _colorScheme.primary.withAlpha(36),
     _colorScheme.surface,
   );
 
+  late final _controller = AnimationController(
+    duration: const Duration(milliseconds: 1000),
+    reverseDuration: const Duration(milliseconds: 1250),
+    value: 0,
+    vsync: this,
+  );
+  late final _railAnimation = RailAnimation(parent: _controller);
+  late final _railFabAnimation = RailFabAnimation(parent: _controller);
+  late final _barAnimation = BarAnimation(parent: _controller);
+
   int selectedIndex = 0;
-  bool wideScreen = false;
+
+  bool controllerInitialized = false;
 
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
 
     final double width = MediaQuery.of(context).size.width;
-    wideScreen = width > 600;
+
+    final AnimationStatus status = _controller.status;
+    if (width > 600) {
+      if (status != AnimationStatus.forward &&
+          status != AnimationStatus.completed) {
+        _controller.forward();
+      }
+    } else {
+      if (status != AnimationStatus.reverse &&
+          status != AnimationStatus.dismissed) {
+        _controller.reverse();
+      }
+    }
+    if (!controllerInitialized) {
+      controllerInitialized = true;
+      _controller.value = width > 600 ? 1 : 0;
+    }
   }
 
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Row(
-        children: [
-          if (wideScreen)
-            DisappearingNavigationRail(
-              selectedIndex: selectedIndex,
-              backgroundColor: _backgroundColor,
-              onDestinationSelected: (index) {
-                setState(() {
-                  selectedIndex = index;
-                });
-              },
-            ),
-          Expanded(
-            child: Container(
-              color: _backgroundColor,
-              child: EmailListView(
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, _) {
+        return Scaffold(
+          body: Row(
+            children: [
+              DisappearingNavigationRail(
+                railAnimation: _railAnimation,
+                railFabAnimation: _railFabAnimation,
                 selectedIndex: selectedIndex,
-                onSelected: (index) {
+                backgroundColor: _backgroundColor,
+                onDestinationSelected: (index) {
                   setState(() {
                     selectedIndex = index;
                   });
                 },
-                currentUser: widget.currentUser,
               ),
-            ),
+              Expanded(
+                child: Container(
+                  color: _backgroundColor,
+                  child: EmailListView(
+                    selectedIndex: selectedIndex,
+                    onSelected: (index) {
+                      setState(() {
+                        selectedIndex = index;
+                      });
+                    },
+                    currentUser: widget.currentUser,
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
-      floatingActionButton: wideScreen
-          ? null
-          : FloatingActionButton(
-        backgroundColor: _colorScheme.tertiaryContainer,
-        foregroundColor: _colorScheme.onTertiaryContainer,
-        onPressed: () {},
-        child: const Icon(Icons.add),
-      ),
-      bottomNavigationBar: wideScreen
-          ? null
-          : DisappearingBottomNavigationBar(
-        selectedIndex: selectedIndex,
-        onDestinationSelected: (index) {
-          setState(() {
-            selectedIndex = index;
-          });
-        },
-      ),
+          floatingActionButton: AnimatedFloatingActionButton(
+            animation: _barAnimation,
+            onPressed: () {},
+            child: const Icon(Icons.add),
+          ),
+          bottomNavigationBar: DisappearingBottomNavigationBar(
+            barAnimation: _barAnimation,
+            selectedIndex: selectedIndex,
+            onDestinationSelected: (index) {
+              setState(() {
+                selectedIndex = index;
+              });
+            },
+          ),
+        );
+      },
     );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
